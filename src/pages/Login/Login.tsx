327025311f2bf16e38fdc9f585fa919faa3ad7dc
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-05-24 22:34:33 +0800

    Add login form

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,65 @@
+import React, { FC, MouseEvent, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+  Button,
+  Link,
+} from '@material-ui/core';
+import { LoginBase } from '../../components';
+import { Eye as EyeIcon, EyeOff as EyeOffIcon } from 'mdi-material-ui';
+
+const Login: FC = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+
+  const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <LoginBase>
+      <Typography variant='h1'>Sign In</Typography>
+      <Typography variant='subtitle1' style={{ marginBottom: '2rem' }}>
+        USER TYPE HERE
+      </Typography>
+      <TextField id='userId' label='User ID' />
+      <TextField
+        id='password'
+        label='Password'
+        type={showPassword ? 'text' : 'password'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position='end'>
+              <IconButton
+                aria-label='toggle password visibility'
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <EyeIcon /> : <EyeOffIcon />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        variant='contained'
+        fullWidth
+        size='large'
+        style={{ marginBottom: '2rem' }}
+      >
+        Sign In
+      </Button>
+      <Typography>
+        <Link component={RouterLink} to='/reset-password'>
+          Forgot password?
+        </Link>
+      </Typography>
+    </LoginBase>
+  );
+};
+
+export default Login;
