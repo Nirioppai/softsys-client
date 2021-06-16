e53f696510f30974be82a332d480c2dfe3537e37
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-06-16 21:22:50 +0800

    Create types for drawer list groups

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export type { AdminLoginTypes } from './AdminLoginTypes';
 export type { ApplicantTypes } from './ApplicantTypes';
+export type { DrawerListGroupsOptions } from './DrawerListGroupsOptions';
 export type { EmployeeTypes } from './EmployeeTypes';
