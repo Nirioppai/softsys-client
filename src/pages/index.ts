eb2ba4414369e4a461a96b29652272840bce8057
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-06-16 16:32:27 +0800

    Replace login with admin login

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@ export { default as Attendance } from './Attendance';
 export { default as Applicants } from './Applicants';
 export { default as Employees } from './Employees';
 export { default as Home } from './Home';
-export { default as Login } from './Login';
+export { default as AdminLogin } from './AdminLogin';
 export { default as NotFound } from './NotFound';
 export { default as OrganizationalChart } from './OrganizationalChart';
 export { default as Portal } from './Portal';
