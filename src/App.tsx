d7f8d14bc3add474a4eb6d0ed4aafdca286d7559
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-10-12 02:03:46 +0800

    Add providers in app entry point

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { ProtectedAdminRoute } from 'routes';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import { theme } from './theme';
+import { SnackbarProvider } from 'notistack';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 // Pages
 import {
@@ -21,47 +24,55 @@ import {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <HelmetProvider>
-        <Helmet
-          titleTemplate='%s - Human Resource Information System'
-          defaultTitle='Human Resource Information System'
-        />
-        <BrowserRouter>
-          <Switch>
-            {/* Auth */}
-            <Route exact path='/' component={Portal} />
-            <Route exact path='/admin/login' component={AdminLogin} />
+      <SnackbarProvider>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <CssBaseline />
+          <HelmetProvider>
+            <Helmet
+              titleTemplate='%s - Human Resource Information System'
+              defaultTitle='Human Resource Information System'
+            />
+            <BrowserRouter>
+              <Switch>
+                {/* Auth */}
+                <Route exact path='/' component={Portal} />
+                <Route exact path='/admin/login' component={AdminLogin} />
 
-            {/* Admin */}
-            <ProtectedAdminRoute exact path='/home' component={Home} />
-            <ProtectedAdminRoute
-              exact
-              path='/applicants'
-              component={Applicants}
-            />
-            <ProtectedAdminRoute
-              exact
-              path='/employees'
-              component={Employees}
-            />
-            <ProtectedAdminRoute
-              exact
-              path='/organizational-chart'
-              component={OrganizationalChart}
-            />
-            <ProtectedAdminRoute exact path='/requests' component={Requests} />
-            <ProtectedAdminRoute
-              exact
-              path='/attendance'
-              component={Attendance}
-            />
+                {/* Admin */}
+                <ProtectedAdminRoute exact path='/home' component={Home} />
+                <ProtectedAdminRoute
+                  exact
+                  path='/applicants'
+                  component={Applicants}
+                />
+                <ProtectedAdminRoute
+                  exact
+                  path='/employees'
+                  component={Employees}
+                />
+                <ProtectedAdminRoute
+                  exact
+                  path='/organizational-chart'
+                  component={OrganizationalChart}
+                />
+                <ProtectedAdminRoute
+                  exact
+                  path='/requests'
+                  component={Requests}
+                />
+                <ProtectedAdminRoute
+                  exact
+                  path='/attendance'
+                  component={Attendance}
+                />
 
-            {/* Misc */}
-            <Route path='*' component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </HelmetProvider>
+                {/* Misc */}
+                <Route path='*' component={NotFound} />
+              </Switch>
+            </BrowserRouter>
+          </HelmetProvider>
+        </MuiPickersUtilsProvider>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
