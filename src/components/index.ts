4c1c5d8af5e0e0c0a953d1986dc3bcb2768f9fdb
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-10-12 02:04:04 +0800

    Index table component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export * from './Dialogs';
+export * from './Info';
 
 export { default as AdminWrapper } from './AdminWrapper';
 export { default as MainLayout } from './MainLayout';
 export { default as LoginBase } from './LoginBase';
 export { default as PageLoader } from './PageLoader';
+export { default as Table } from './Table';
