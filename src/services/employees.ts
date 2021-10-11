db8d41c928493908fecb19d4eb497ec5deb007d4
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-10-12 02:06:28 +0800

    Add employee mock API requests

--- src/services/employees.ts
+++ src/services/employees.ts
@@ -4,3 +4,15 @@ import { baseUrl } from 'services';
 export const getEmployees = () => {
   return axios.get(`${baseUrl}/employees.json`);
 };
+
+export const postEmployee = (data: any) => {
+  return axios.post(`https://jsonplaceholder.typicode.com/users`, data);
+};
+
+export const putEmployee = (_id: string, data: any) => {
+  return axios.put(`https://jsonplaceholder.typicode.com/users/${_id}`, data);
+};
+
+export const deleteEmployee = (_id: string) => {
+  return axios.delete(`https://jsonplaceholder.typicode.com/users/${_id}`);
+};
