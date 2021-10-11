b46201887082d69ddc19bb33b7bdf4cc09d2d34f
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-10-11 21:14:03 +0800

    Add table component

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,7 @@ export * from './IAdminLogin';
 export * from './IApplicant';
 export * from './IDrawerListGroups';
 export * from './IEmployee';
+export * from './TableProps';
+
+// react-table
+export * from './react-table-config';
