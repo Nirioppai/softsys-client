3ffc20350ca48cfc68f3e5e395ee9f696d6059f1
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2021-10-12 17:13:39 +0800

    Add employee information types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export * from './IAdminLogin';
 export * from './IApplicant';
 export * from './IDrawerListGroups';
 export * from './IEmployee';
+export * from './IEmployeeInformation';
 export * from './TableProps';
 
 // react-table
