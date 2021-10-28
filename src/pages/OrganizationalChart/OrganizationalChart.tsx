8c56940a12090b076c8b863c229da8e4095aa59a
Edren Añonuevo <68000960+eanonuevo@users.noreply.github.com>
2021-10-28 21:27:00 +0800

    react-org-chart added and initial setup

--- src/pages/OrganizationalChart/OrganizationalChart.tsx
+++ src/pages/OrganizationalChart/OrganizationalChart.tsx
@@ -2,6 +2,37 @@ import { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Typography } from '@material-ui/core';
 import { AdminWrapper } from 'components';
+// @ts-ignore
+import OrgTree from 'react-org-tree';
+
+const horizontal = false;
+const collapsable = true;
+const expandAll = true;
+
+const data = {
+    id: 0,
+    label: 'Edren - Hot guys',
+    children: [{
+        id: 1,
+        label: 'Loui',
+        children: [{
+            id: 4,
+            label: 'Nico'
+        }, {
+            id: 5,
+            label: 'Franz'
+        }, {
+            id: 6,
+            label: 'Carlo'
+        }]
+    }, {
+        id: 2,
+        label: 'Gemuel'
+    }, {
+        id: 3,
+        label: 'Doc A - Aliping Sanguiguilid'
+    }]
+}
 
 const OrganizationalChart: FC = () => {
   return (
@@ -11,6 +42,15 @@ const OrganizationalChart: FC = () => {
         Organizational Chart
       </Typography>
       <Typography>Under construction</Typography>
+
+      <OrgTree
+        data={data}
+        horizontal={horizontal}
+        collapsable={collapsable}
+        expandAll={expandAll}
+      >'
+      </OrgTree>
+
     </AdminWrapper>
   );
 };
